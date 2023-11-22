695d7c5abf0192f9f318e8876aa3bf006557ef5c
Angel <22824872+Zyplos@users.noreply.github.com>
2023-11-21 20:26:23 -0600

    use .env file instead of hardcoded secret values

--- functions/githubcontributors.js
+++ functions/githubcontributors.js
@@ -1,55 +1,60 @@
+require("dotenv").config();
 // Octokit.js
 // https://github.com/octokit/core.js#readme
 const { map } = require("cheerio");
-const {grabHTMLFromSite} = require ("./common.js");
+const { grabHTMLFromSite } = require("./common.js");
 
 // npm install @octokit/core
 const { Octokit } = require("@octokit/core");
 // or: import { Octokit } from "@octokit/core";
 
 async function getContributors() {
-    try {
-        // create an octokit (github api) instance
-        const octokit = new Octokit({
-            // put the token here
-            auth: ''
-        })
-        
-        // getting the contributors from the backend repo
-        let contributors = await octokit.request('GET /repos/{owner}/{repo}/contributors', {
-            owner: 'gdscuic',
-            repo: 'gdsc-uic-web-backend',
-            per_page: 100,
-            page: 1
-        })
+  if (!process.env.GITHUB_TOKEN) {
+    throw new Error("GITHUB_TOKEN not set in .env file");
+  }
 
-        // getting the contributors from the frontend repo
-        let contributors2 = await octokit.request('GET /repos/{owner}/{repo}/contributors', {
-            owner: 'gdscuic',
-            repo: 'gdsc-uic-web-frontend',
-            per_page: 100,
-            page: 1
-        })
+  try {
+    // create an octokit (github api) instance
+    const octokit = new Octokit({
+      // put the token here
+      auth: process.env.GITHUB_TOKEN,
+    });
 
-        // creating a map to have the unique contributors and their images
-        const contri_and_image = new Map();
+    // getting the contributors from the backend repo
+    let contributors = await octokit.request("GET /repos/{owner}/{repo}/contributors", {
+      owner: "gdscuic",
+      repo: "gdsc-uic-web-backend",
+      per_page: 100,
+      page: 1,
+    });
 
-        // adding each contributor and their image to the map
-        contributors.data.forEach(function(entry){
-            // console.log(entry.login);
-            contri_and_image.set(entry.login, entry.avatar_url);
-        });
+    // getting the contributors from the frontend repo
+    let contributors2 = await octokit.request("GET /repos/{owner}/{repo}/contributors", {
+      owner: "gdscuic",
+      repo: "gdsc-uic-web-frontend",
+      per_page: 100,
+      page: 1,
+    });
 
-        contributors2.data.forEach(function(entry){
-            // console.log(entry.login);
-            contri_and_image.set(entry.login, entry.avatar_url);
-        });
+    // creating a map to have the unique contributors and their images
+    const contri_and_image = new Map();
 
-        return contri_and_image;
-    } catch (e) {
-        console.log(e);
-        return null;
-    }
+    // adding each contributor and their image to the map
+    contributors.data.forEach(function (entry) {
+      // console.log(entry.login);
+      contri_and_image.set(entry.login, entry.avatar_url);
+    });
+
+    contributors2.data.forEach(function (entry) {
+      // console.log(entry.login);
+      contri_and_image.set(entry.login, entry.avatar_url);
+    });
+
+    return contri_and_image;
+  } catch (e) {
+    console.log(e);
+    return null;
+  }
 }
 
-module.exports = getContributors
+module.exports = getContributors;

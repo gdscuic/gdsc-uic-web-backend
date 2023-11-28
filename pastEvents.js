e2d00a2d179ab9101792e0eab23f0ef5de925543
Guillermo <150206090+NinjaDogeBro@users.noreply.github.com>
2023-11-27 21:57:14 -0600

    Update and rename index.js to pastEvents.js

--- pastEvents.js
+++ pastEvents.js
@@ -2,28 +2,20 @@
 const {grabHTMLFromSite} = require ("./common.js");
 const cheerio = require ("cheerio");
 
-
-
 async function getPastEvents(){
     let pastEvents = [];
     try {
         const html = await grabHTMLFromSite();
         if (!html) return null;
         let $ = await cheerio.load(html);
-        const website = "#mu9W6Ekd-k3"
-        $(website.find("dynamic-text")).each((i,e) => {
-            pastEvents.push($(e).text().trim())
-            //''' console.log(e); '''
+        const website = $("#mu9W6Ekd-k3");
+        const stuff = website.find(".dynamic-text");
+        stuff.each((i,e) => {
+            pastEvents.push($(e).text().trim());
         });
     }catch(e) {
             console.log(e)
         };
         return pastEvents;
     };
-
-// app.get("/oldData", (req, res) => {
-//     res.send('oldData');
-// });
 module.exports = getPastEvents;
-
-// app.listen(port, () => console.log("Server running") );

1bb35f2ab4338f01e33b32e49391fff156dc4df5
Angel <22824872+Zyplos@users.noreply.github.com>
2023-11-29 13:10:51 -0600

    simple express server

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// TODO set up an express server of some kind so frontend can grab data from here
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(bodyParser.json());
+
+// enabling CORS for all requests
+const corsOptions = {
+  origin: ['http://localhost:' + PORT, "https://gdscuic.dev"],
+};
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send({
+    message: "Works!"
+  });
+});
+
+// starting the server
+app.listen(PORT, () => {
+  console.log('listening on port ' + PORT);
+});
